844bd723d3e87d28fd8e5875fa0492681cbf27b1
Ayush Arora <52823964+aroraayush14@users.noreply.github.com>
2024-03-12 12:56:19 +0530

    Update synapse_test.py

--- synapse_test.py
+++ synapse_test.py
@@ -1,6 +1,31 @@
 import os
 from connection import connect
 
+def get_table_sql_query(schema_name):
+    table_sql_query = f"""SELECT 
+    c.table_name,c.column_name,
+    c.data_type,
+    c.character_maximum_length,
+    CASE 
+    WHEN UPPER(c.data_type) = 'DECIMAL' THEN '['+c.column_name+']'+ ' ' + '['+c.data_type+']' + ' (' + COALESCE(CAST(c.numeric_precision AS VARCHAR(20)),'') +','+COALESCE(CAST(c.numeric_scale AS VARCHAR(20)),'')+')'
+    WHEN UPPER(c.data_type) = 'VARCHAR' or UPPER(c.data_type) = 'NVARCHAR' or UPPER(c.data_type) = 'CHAR' THEN '['+c.column_name+']'+ ' ' + '['+c.data_type+']'+'('+CAST(c.character_maximum_length AS VARCHAR(20))+')'
+    WHEN UPPER(c.data_type) = 'DATETIME2'  THEN '['+c.column_name+']'+ ' ' + '['+c.data_type+']'+'(7)'
+    ELSE '['+c.column_name+']'+ ' ' + '['+c.data_type+']' END as derivedcolumn,
+    CASE WHEN c.collation_name IS NOT NULL THEN 'COLLATE '+c.collation_name ELSE c.collation_name END AS collation_name,
+    et.location
+    FROM INFORMATION_SCHEMA.COLUMNS c
+    LEFT JOIN sys.external_tables et
+    ON TRIM(c.table_name) = TRIM(et.name)
+    WHERE c.table_schema = '{schema_name}'
+    ORDER BY c.table_name, c.ORDINAL_POSITION asc"""
+    return table_sql_query
+
+def get_view_sql_query(schema_name):
+    view_sql_query = f"""SELECT * FROM INFORMATION_SCHEMA.VIEWS
+    WHERE table_schema = '{schema_name}' 
+    ORDER BY table_name asc"""
+    return view_sql_query
+
 def get_table_ddl(schema_name, table_name, cursor):
     ddl_query = f"""SELECT 
         c.column_name,
@@ -32,10 +57,7 @@ def get_table_ddl(schema_name, table_name, cursor):
     return ddl
 
 def create_table_ddl_files(schema_name, cursor):
-    table_sql_query = f"""SELECT DISTINCT c.table_name
-        FROM INFORMATION_SCHEMA.COLUMNS c
-        WHERE c.table_schema = '{schema_name}'"""
-
+    table_sql_query = get_table_sql_query(schema_name)
     cursor.execute(table_sql_query)
     table_names = [row[0] for row in cursor.fetchall()]
 
@@ -53,9 +75,5 @@ def create_table_ddl_files(schema_name, cursor):
 schema_name = input('Enter Schema Name: ')
 
 # Get table SQL query and execute
-table_sql_query = get_table_sql_query(schema_name)
 cursor = connect.cursor()
-cursor.execute(table_sql_query)
-
-# Create table DDL files
 create_table_ddl_files(schema_name, cursor)
